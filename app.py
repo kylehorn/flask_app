75eb0a5d49c1a3ea3b94d6d47e6437ffabc5cf98
Kyle Horn <kylehorn@me.com>
2014-09-06 15:20:20 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, render_template
+from flask.ext import assets
+
+app = Flask(__name__)
+
+env = assets.Environment(app)
+
+env.load_path = [
+    os.path.join(os.path.dirname(__file__), 'sass'),
+    os.path.join(os.path.dirname(__file__), 'coffee'),
+    os.path.join(os.path.dirname(__file__), 'bower_components'),
+]
+
+env.register(
+    'js_all',
+    assets.Bundle(
+        'jquery/dist/jquery.min.js',
+        assets.Bundle(
+            'all.coffee',
+            filters=['coffeescript']
+        ),
+        output='js_all.js'
+    )
+)
+
+env.register(
+    'css_all',
+    assets.Bundle(
+        'all.sass',
+        filters='sass',
+        output='css_all.css'
+    )
+)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
